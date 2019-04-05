f9675947824ec04f493e2a18cd5fc21c0fef9d4b
Sam Singer <samuel.singer1@gmail.com>
2019-04-05 15:07:24 -0700

    Transistion Ninjas to a function component

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,25 +1,22 @@
-import React, {Component} from 'react'
+import React from 'react';
 
-class Ninjas extends Component {
-
-    render () {
-        const { ninjas } = this.props;
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div className="ninjas" key={ninja.id}>
-                    <p>{ninja.name}</p>
-                    <p>{ninja.age}</p>
-                    <p>{ninja.skill}</p>
-                </div>
-            )
-        })
-
+const ninjas = (props) => {
+    const { ninjas } = props;
+    const ninjaList = ninjas.map(ninja => {
         return (
-            <div className="ninjas">
-                { ninjaList }
+            <div className="ninjas" key={ninja.id}>
+                <p>{ninja.name}</p>
+                <p>{ninja.age}</p>
+                <p>{ninja.skill}</p>
             </div>
         )
-    }
+    });
+
+    return (
+        <div className="ninjas">
+            { ninjaList }
+        </div>
+    );
 }
 
-export default Ninjas
+export default ninjas;
