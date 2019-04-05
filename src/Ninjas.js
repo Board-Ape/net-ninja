99970c69d374aabd1cd629888d4e2973e827bada
Sam Singer <samuel.singer1@gmail.com>
2019-04-05 15:16:57 -0700

    Three different ways to conditinally render

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,20 +1,44 @@
 import React from 'react';
 
-const ninjas = (props) => {
-    const { ninjas } = props;
-    const ninjaList = ninjas.map(ninja => {
-        return (
-            <div className="ninjas" key={ninja.id}>
-                <p>{ninja.name}</p>
-                <p>{ninja.age}</p>
-                <p>{ninja.skill}</p>
-            </div>
-        )
-    });
+const ninjas = ({ ninjas }) => {
+
+    // const ninjaList = ninjas.map(ninja => {
+    //     if (ninja.age > 25) {
+    //         return (
+    //             <div className="ninjas" key={ninja.id}>
+    //                 <p>{ninja.name}</p>
+    //                 <p>{ninja.age}</p>
+    //                 <p>{ninja.skill}</p>
+    //             </div>
+    //         )
+    //     } else {
+    //         return null
+    //     }
+    // });
+
+    // const ninjaList = ninjas.map(ninja => {
+    //     return ninja.age > 25 ? (
+    //         <div className="ninjas" key={ninja.id}>
+    //             <p>{ninja.name}</p>
+    //             <p>{ninja.age}</p>
+    //             <p>{ninja.skill}</p>
+    //         </div>
+    //     ) : null
+    // })
 
     return (
         <div className="ninjas">
-            { ninjaList }
+            { 
+                ninjas.map(ninja => {
+                    return ninja.age > 25 ? (
+                        <div className="ninjas" key={ninja.id}>
+                            <p>{ninja.name}</p>
+                            <p>{ninja.age}</p>
+                            <p>{ninja.skill}</p>
+                        </div>
+                ) : null
+        })
+                 }
         </div>
     );
 }
